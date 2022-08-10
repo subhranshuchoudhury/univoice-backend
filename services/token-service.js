af8e79835c1c568993fee2c609b95a4d4354ebb2
subhranshuchoudhury <subhransuchoudhury00@gmail.com>
2022-08-10 18:03:50 +0530

    88u9994

--- services/token-service.js
+++ services/token-service.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 require("dotenv").config();
-const accessTokenSecret = process.env.JWT_ACCESS_TOKEN;
-const refreshTokenSecret = process.env.JWT_REFRESH_TOKEN;
+const accessTokenSecret = "9e03fddc477f8dddf89ca6b608d1c6cccdc882ccd104dbafcdb02ff8edd419296937b1b6562db403c0be150a0a432f70c5e13cea0b572d9ac143ac7ab0cddc3a";
+const refreshTokenSecret = "3be8aba5089a46db14d7ac02cf37b0eba85bf403b767af6d04cb9cea3a7832450e13be06c40652df6b6d4decd828c395e4cf06d0f082bd29764f760e7ee8737a";
 const refreshModel = require('../models/refresh-model');
 class TokenService {
     generateTokens(payload) {
