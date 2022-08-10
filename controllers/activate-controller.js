6d53f1093740d1c9035125db877b1201f48c5a96
subhranshuchoudhury <subhransuchoudhury00@gmail.com>
2022-08-10 22:55:52 +0530

    lets see

--- controllers/activate-controller.js
+++ controllers/activate-controller.js
@@ -11,24 +11,24 @@ class ActivateController {
             res.status(400).json({ message: 'All fields are required!' });
         }
 
-        // // Image Base64
-        // const buffer = Buffer.from(
-        //     avatar.replace(/^data:image\/(png|jpg|jpeg);base64,/, ''),
-        //     'base64'
-        // );
-        // const imagePath = `${Date.now()}-${Math.round(
-        //     Math.random() * 1e9
-        // )}.png`;
-        // // 32478362874-3242342342343432.png
+        // Image Base64
+        const buffer = Buffer.from(
+            avatar.replace(/^data:image\/(png|jpg|jpeg);base64,/, ''),
+            'base64'
+        );
+        const imagePath = `${Date.now()}-${Math.round(
+            Math.random() * 1e9
+        )}.png`;
+        // 32478362874-3242342342343432.png
 
-        // try {
-        //     const jimResp = await Jimp.read(buffer);
-        //     jimResp
-        //         .resize(150, Jimp.AUTO)
-        //     // .write(path.resolve(__dirname, `../storage/${imagePath}`));
-        // } catch (err) {
-        //     res.status(500).json({ message: 'Could not process the image' });
-        // }
+        try {
+            const jimResp = await Jimp.read(buffer);
+            jimResp
+                .resize(150, Jimp.AUTO)
+                .write(path.resolve(__dirname, `../storage/${imagePath}`));
+        } catch (err) {
+            res.status(500).json({ message: 'Could not process the image' });
+        }
 
         const userId = req.user._id;
         // Update user
