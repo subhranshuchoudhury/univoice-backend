4c556c5a15feb27faf9a8cc9d5a2a9e14cd2fc8f
subhranshuchoudhury <subhransuchoudhury00@gmail.com>
2022-08-10 22:28:12 +0530

    image storage clea0787

--- routes.js
+++ routes.js
@@ -4,14 +4,24 @@ const activateController = require('./controllers/activate-controller');
 const authMiddleware = require('./middlewares/auth-middleware');
 const roomsController = require('./controllers/rooms-controller');
 
+// router.post('/api/send-otp', authController.sendOtp);
+// router.post('/api/verify-otp', authController.verifyOtp);
+// router.post('/api/activate', authMiddleware, activateController.activate);
+// router.get('/api/refresh', authController.refresh);
+// router.post('/api/logout', authMiddleware, authController.logout);
+// router.post('/api/rooms', authMiddleware, roomsController.create);
+// router.get('/api/rooms', authMiddleware, roomsController.index);
+// router.get('/api/rooms/:roomId', authMiddleware, roomsController.show);
+// router.get('/api/test', (req, res) => res.json({ msg: 'OK' }));
+
 router.post('/api/send-otp', authController.sendOtp);
 router.post('/api/verify-otp', authController.verifyOtp);
-router.post('/api/activate', authMiddleware, activateController.activate);
+router.post('/api/activate', activateController.activate);
 router.get('/api/refresh', authController.refresh);
-router.post('/api/logout', authMiddleware, authController.logout);
-router.post('/api/rooms', authMiddleware, roomsController.create);
-router.get('/api/rooms', authMiddleware, roomsController.index);
-router.get('/api/rooms/:roomId', authMiddleware, roomsController.show);
+router.post('/api/logout', authController.logout);
+router.post('/api/rooms', roomsController.create);
+router.get('/api/rooms', roomsController.index);
+router.get('/api/rooms/:roomId', roomsController.show);
 router.get('/api/test', (req, res) => res.json({ msg: 'OK' }));
 
 module.exports = router;
