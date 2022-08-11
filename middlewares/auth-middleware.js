8f449aaabd9c37014a83fa4c38d7af1102f0d10b
subhranshuchoudhury <subhransuchoudhury00@gmail.com>
2022-08-11 20:54:00 +0530

    okiu

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -2,14 +2,14 @@ const tokenService = require('../services/token-service');
 
 module.exports = async function (req, res, next) {
     try {
-        // const { accessToken } = req.cookies;
-        // if (!accessToken) {
-        //     throw new Error();
-        // }
-        // const userData = await tokenService.verifyAccessToken(accessToken);
-        // if (!userData) {
-        //     throw new Error();
-        // }
+        const { accessToken } = req.cookies;
+        if (!accessToken) {
+            throw new Error();
+        }
+        const userData = await tokenService.verifyAccessToken(accessToken);
+        if (!userData) {
+            throw new Error();
+        }
         req.user = userData;
         next();
     } catch (err) {
