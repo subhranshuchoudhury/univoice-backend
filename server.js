bd18167946781dbe463840b53c2803a2d255ef30
subhranshuchoudhury <subhransuchoudhury00@gmail.com>
2022-08-12 10:11:16 +0530

    cors option3

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const corsOption = {
 };
 app.use(cors(corsOption));
 
-
+app.use('/storage', express.static('storage'));
 
 const PORT = process.env.PORT || 5000;
 DbConnect();
@@ -31,7 +31,7 @@ app.use(express.json({ limit: '8mb' }));
 app.use(router);
 
 app.get('/', (req, res) => {
-    res.send('SERVER WORKING FINE, HELLO FROM SUBHRANSHU!');
+    res.send('SERVER WORKING FINE! UNIVOICE');
 });
 
 // Sockets
