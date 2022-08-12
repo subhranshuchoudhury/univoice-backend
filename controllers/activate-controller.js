a3d81b8e05448a743b59d09bd5c2b38d8bab942a
subhranshuchoudhury <subhransuchoudhury00@gmail.com>
2022-08-12 09:27:32 +0530

    hide enter name3

--- controllers/activate-controller.js
+++ controllers/activate-controller.js
@@ -1,34 +1,34 @@
-const Jimp = require('jimp');
-const path = require('path');
+// const Jimp = require('jimp');
+// const path = require('path');
 const userService = require('../services/user-service');
 const UserDto = require('../dtos/user-dto');
 
 class ActivateController {
     async activate(req, res) {
         // Activation logic
-        const { name, avatar } = req.body;
-        if (!name || !avatar) {
+        const { name } = req.body;
+        if (!name) {
             res.status(400).json({ message: 'All fields are required!' });
         }
 
         // Image Base64
-        const buffer = Buffer.from(
-            avatar.replace(/^data:image\/(png|jpg|jpeg);base64,/, ''),
-            'base64'
-        );
-        const imagePath = `${Date.now()}-${Math.round(
-            Math.random() * 1e9
-        )}.png`;
-        // 32478362874-3242342342343432.png
+        // const buffer = Buffer.from(
+        //     avatar.replace(/^data:image\/(png|jpg|jpeg);base64,/, ''),
+        //     'base64'
+        // );
+        // const imagePath = `${Date.now()}-${Math.round(
+        //     Math.random() * 1e9
+        // )}.png`;
+        // // 32478362874-3242342342343432.png
 
-        try {
-            const jimResp = await Jimp.read(buffer);
-            jimResp
-                .resize(150, Jimp.AUTO)
-            // .write(path.resolve(__dirname, `../storage/${imagePath}`));
-        } catch (err) {
-            res.status(500).json({ message: 'Could not process the image' });
-        }
+        // try {
+        //     const jimResp = await Jimp.read(buffer);
+        //     jimResp
+        //         .resize(150, Jimp.AUTO)
+        //     // .write(path.resolve(__dirname, `../storage/${imagePath}`));
+        // } catch (err) {
+        //     res.status(500).json({ message: 'Could not process the image' });
+        // }
 
         const userId = req.user._id;
         // Update user
